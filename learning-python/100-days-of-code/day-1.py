3ef9606b504d06148d63d54b1e2cc79dcc361c06
Christian Jurilla <nogicrockforge@outlook.com>
2022-01-15 18:37:10 +0800

    Added 100 days of code and django-practice

---
+++ learning-python/100-days-of-code/day-1.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Generator")
+
+city_name = input("What's the name of the city your grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+
+print(f"Your band name could be {city_name} {pet_name}")
+
+if __name__ == 'main':
+    band_name_generator()
