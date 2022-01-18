f9c06e773656d97a7647c73d30398ab2e1a15cef
Christian Jurilla <nogicrockforge@outlook.com>
2022-01-18 19:04:23 +0800

    Updated files

--- learning-python/practice-fast-api/main.py
+++ learning-python/practice-fast-api/main.py
@@ -1,6 +1,15 @@
 from enum import Enum
-
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+import math
+
+fake_items_db = [
+    {'item_name': 'Foo'},
+    {'item_name': 'Bar'},
+    {'item_name': 'Baz'},
+]
 
 class ModelName(str, Enum):
     """A class used to represent a model name
@@ -15,15 +24,101 @@ class ModelName(str, Enum):
     resnet = 'resnet'
     lenet = 'lenet'
 
+
+class Item(BaseModel):
+    """A class used to represent an item
+
+    Attributes
+    ----------
+    name : str
+        The name of the item.
+
+    description : str, optional, default=None
+        The description of the item.
+
+    price : float
+        The price of the item.
+
+    tax : float, optional, default=None
+        The tax for the item.
+    """
+
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
 app = FastAPI()
 
 @app.get('/')
 async def root():
     return {'message': 'Hello World'}
 
+@app.get('/items/')
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+@app.post('/items')
+async def create_item(item: Item):
+    item_dict = item.dict()
+
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({'price_with_tax': price_with_tax})
+
+    return item_dict
+
+@app.put('/items/{item_id}')
+async def create_item(item_id: int, item: Item):
+    return {'item_id': item_id, **item.dict()}
+
 @app.get('/items/{item_id}')
-async def read_item(item_id: int):
-    return {'item_id': item_id}
+async def read_user_item(item_id: str,
+                         needy: str,
+                         skip: int = 0,
+                         limit: Optional[int] = None):
+    """Read user item
+
+    Parameters
+    ----------
+    item_id : str
+        The item ID
+
+    needy : str
+        Determine if this is needy
+
+    skip : int, default=0
+        The skip value
+
+    limit : int, default=None
+        The limit for the data
+
+    Returns
+    -------
+    dict
+        The item with values
+    """
+
+    str = {
+        'item_id': item_id,
+        'needy': needy,
+        'skip': skip,
+        'limit': limit,
+    }
+
+    return item
+
+# @app.get('/users/{user_id}/items/{item_id}')
+# async def read_user_item(user_id: int, item_id: int, q: Optional[str] = None,
+#                          short: bool = False):
+#     item = {'item_id': item_id, 'owner_id': user_id}
+#     if q:
+#         item.update({'q': q})
+#     if not short:
+#         item.update(
+#             {'description': 'This is an amazing item that has a long description'}
+#         )
+#     return item
 
 """Notice the two routes
 
@@ -76,3 +171,4 @@ async def get_model(model_name: ModelName):
 @app.get('/files/{file_path:path}')
 async def read_file(file_path: str):
     return {'file_path': file_path}
+
