3f046d1dde8ee0694c58185e62026348cc7400cd
Christian Jurilla <chan-isentia@PHMAC83921s-MacBook-Pro.local>
2022-02-21 16:29:10 +0800

    Updated files

--- learning-opensearch/poke-app/src/app/app.component.ts
+++ learning-opensearch/poke-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'poke-app';
+
+  pokemon?: any;
+
+  pokemonId: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  getPokemon() {
+    this.http.get(`http://localhost:8000/pokemon/${this.pokemonId}`)
+      .subscribe(response => {
+        this.pokemon = response;
+        this.pokemon['image'] = this.pokemon.id
+          .toString()
+          .padStart(3, 0);
+      });
+  }
 }
