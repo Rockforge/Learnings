576fe1391ef118768c84fa3182bfb366f0420048
Christian Jurilla <chan-isentia@PHMAC83921s-MacBook-Pro.local>
2022-02-21 16:28:59 +0800

    Added CORS middleware

--- learning-opensearch/main.py
+++ learning-opensearch/main.py
@@ -1,3 +1,21 @@
+from typing import Optional
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.service import pokeService
 
-print(pokeService.get('pokedex', 999))
+
+app = FastAPI()
+
+origins = ["http://localhost:4200"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/pokemon/{pokemon_id}')
+def get_pokemon(pokemon_id: int):
+    return pokeService.get('pokedex', pokemon_id)['_source']
